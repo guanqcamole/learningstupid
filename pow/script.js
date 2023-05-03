7d10db8b3bc62965cccae49d2d7e07be35258757
Quang Pham <quangpham4071@gmail.com>
2023-05-02 21:31:23 -0400

    Accounted for all cases of input

--- pow/script.js
+++ pow/script.js
@@ -6,33 +6,41 @@ function onClick() {
     updateLabel();
 }
 
+//Check if inputs are empty
 function inputsAreEmpty() {
-    return (isNaN(getX()) || isNaN(getN()));
-    // return true;
+    return (getNum(inputs[0].value) === "" || getNum(inputs[1].value) === "");
 }
 
 function calculate(x, n) {
-    if (x === n && x === 0) return undefined;
-
-    let result = 1;
-    for(let i = 0; i < n; i++) {
-        result = result * x;
-    }
-    return result;
+    return x**n;
 }
 
 function updateLabel() {
-    label.textContent = "result is: " + calculate(getX(), getN());
+    label.textContent = "result is: " +
+     calculate(getNum(inputs[0].value), getNum(inputs[1].value));
 }
 
-function getX() {
-    return parseInt(inputs[0].value);
-}
-function getN() {
-    return parseInt(inputs[1].value);
+function getNum(a) {
+    //if input is not a fraction
+    //if a is a string: "5", then it returns a
+    //if a is a string: "5/2", then it continues
+    if (!(isNaN(+a))) 
+        return a;
+    //if input is a fraction
+    const fraction = a;
+    const parts = fraction.split('/');
+    const numerator = parseFloat(parts[0]);
+    const denominator = parseFloat(parts[1]);
+    const result = numerator / denominator;
+    return result;
 }
 
-let inputs = document.querySelectorAll('input');
-let label = document.querySelector('p');
-let button = document.querySelector('button');
+let inputs = document.querySelectorAll('#inpPower'); //Takes string input
+let label = document.querySelector('#label');
+let button = document.querySelector('#myButton');
 button.addEventListener('click', onClick);
+
+
+//Calculate string input
+//Get float input
+//Calculate and return power
