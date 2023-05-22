322ce9e0db3620963894eaf5f766f13a4e26ea2a
Quang Pham <quangpham4071@gmail.com>
2023-05-22 00:13:27 -0400

    changed structure of functions to be more readable

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,79 +1,73 @@
-// function compareChoice(computerChoice, playerChoice) {
-//     //0 is rock
-//     //1 is paper
-//     //2 is scissors
-//     let playerWins = `You win, ${playerChoice} beats ${computerChoice}`;
-//     let computerWins = `Computer wins, ${computerChoice} beats ${playerChoice}`;
+const game = () => {
+    const rockBtn = document.querySelector('.rock');
+    const paperBtn = document.querySelector('.paper');
+    const scissorsBtn = document.querySelector('.scissors');
+    const playerChoice = [rockBtn, paperBtn, scissorsBtn];
+    const computerChoice = ['rock', 'paper', 'scissors'];
 
-//     if (computerChoice === playerChoice) {playerScore++; computerScore++; return `It's a tie, you both chose ${computerChoice}`;} //tie
-//     if (computerChoice === 'rock') {
-//         if (playerChoice === 'paper') {playerScore++; return playerWins;} //you win
-//         if (playerChoice === 'scissors') {computerScore++; return computerWins;} //computer wins
-//     }
-//     if (computerChoice === 'paper') {
-//         if (playerChoice === 'scissors') {playerScore++; return playerWins;} //you win
-//         if (playerChoice === 'rock') {computerScore++; return computerWins;} //computer wins
-//     }
-//     if (computerChoice === 'scissors') {
-//         if (playerChoice === 'rock') {playerScore++; return playerWins;} //you win
-//         if (playerChoice === 'paper') {computerScore++; return computerWins;} //computer wins
-//     }
-// }
-
-
-
-// let button = document.querySelectorAll('.playerChoice');
-
-// for (i of button) {
-//     i.addEventListener('click', function() {
-//         document.querySelector('.msg').innerHTML = compareChoice(getComputerChoice(), this.innerHTML);
-//         // document.querySelector('.score').innerHTML = 
-//     });
-// }
-
-
-// function game(computerScore, playerScore) {
-
-// }
-
-
-
-
-//correct
-function getComputerChoice() {
-    let computerChoice = (Math.floor(Math.random()*3));
-    switch (computerChoice) {
-        case (0): return 'rock';
-        case (1): return 'paper';
-        case (2): return 'scissors';
+    const playGame = () => {
+        playerChoice.forEach((playerPlays) => {
+            playerPlays.addEventListener('click', () => {
+                let computerPlays = Math.floor(Math.random()*3);
+                computerPlays = computerChoice[computerPlays];
+                const roundResult = oneRound(playerPlays.value, computerPlays);
+                keepScore(roundResult);
+            })
+        })
     }
+
+    const oneRound = (playerChoice, computerChoice) => {
+        const message = document.querySelector('.msg');
+        //tie
+        if (computerChoice === playerChoice) {
+            message.innerHTML = `It's a tie, you both played ${playerChoice}`;
+            return 0;
+        }
+        if (computerChoice === 'rock') {
+            //you win
+            if (playerChoice === 'paper') {
+                message.innerHTML = `You win, ${playerChoice} beats ${computerChoice}`;
+                return 1;
+            } 
+            //computer wins
+            if (playerChoice === 'scissors') {
+                message.innerHTML = `Computer wins, ${computerChoice} beats ${playerChoice}`;
+                return -1;
+            } 
+        }
+        if (computerChoice === 'paper') {
+             //you win
+            if (playerChoice === 'scissors') {
+                message.innerHTML = `You win, ${playerChoice} beats ${computerChoice}`;
+                return 1;
+            }
+            //computer wins
+            if (playerChoice === 'rock') {
+                message.innerHTML = `Computer wins, ${computerChoice} beats ${playerChoice}`
+                return -1;
+            } 
+        }
+        if (computerChoice === 'scissors') {
+            //you win
+            if (playerChoice === 'rock') {
+                message.innerHTML = `You win, ${playerChoice} beats ${computerChoice}`;
+                return 1;
+            } 
+            //computer wins
+            if (playerChoice === 'paper') {
+                message.innerHTML = `Computer wins, ${computerChoice} beats ${playerChoice}`
+                return -1;
+            } 
+        }
+
+    }
+
+    const keepScore = (round) => {
+
+    }
+
+    playGame();
 }
 
-function getPlayerChoice() {
-    let buttons = document.querySelectorAll('.playerChoice');
-    buttons.forEach((button) => {
-        button.addEventListener('click', function() {
-            return(button.value);
-        });
-    })
-}
+game();
 
-function compareChoices(playerChoice, computerChoice) {
-    if (computerChoice === playerChoice) return 0; //tie
-    if (computerChoice === 'rock') {
-        if (playerChoice === 'paper') {return 1;} //you win
-        if (playerChoice === 'scissors') {return -1;} //computer wins
-    }
-    if (computerChoice === 'paper') {
-        if (playerChoice === 'scissors') {return 1;} //you win
-        if (playerChoice === 'rock') {return -1;} //computer wins
-    }
-    if (computerChoice === 'scissors') {
-        if (playerChoice === 'rock') {return 1;} //you win
-        if (playerChoice === 'paper') {return -1;} //computer wins
-    }
-}
-
-function keepScore() {
-    
-}
